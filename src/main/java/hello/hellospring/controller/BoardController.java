a009148f0a4ad5c9f1a9f9927bf2249407a9ee29
Leesungsup <tmdtjq0116@naver.com>
2022-07-21 23:11:49 +0900

    board list

--- src/main/java/hello/hellospring/controller/BoardController.java
+++ src/main/java/hello/hellospring/controller/BoardController.java
@@ -4,6 +4,7 @@ import hello.hellospring.entity.Board;
 import hello.hellospring.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -22,5 +23,10 @@ public class BoardController {
         System.out.println(board.getTitle());
         System.out.println("내용"+board.getContent());
         return "";
+    }
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list",boardService.boardList());
+        return "boardlist";
     }
 }
